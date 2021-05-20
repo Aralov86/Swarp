59bfeb7e526ecbc03ae1f5244ede2aaab88fa247
mishycz <aralov199186@gmail.com>
2021-05-20 19:08:52 +0300

    task2 - подключил базу данных, сделал вывод сообщений на экран

---
+++ src/main/java/com/example/Swarp/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.Swarp.repos;
+
+import com.example.Swarp.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}

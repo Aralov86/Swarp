574692ac2c2f0d6c5e66628bf6e0f618b75f18be
aralov91 <aralov199186@gmail.com>
2021-05-26 18:48:48 +0300

    Добавил имя автора сообщений в список сообщений

--- src/main/java/com/example/Swarp/controller/MainController.java
+++ src/main/java/com/example/Swarp/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.Swarp.controller;
 
 import com.example.Swarp.domain.Message;
+import com.example.Swarp.domain.User;
 import com.example.Swarp.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,14 +30,19 @@ public class MainController {
         return "main";
     }
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
 
         model.put("messages", messages);
+
         return "main";
     }
 
